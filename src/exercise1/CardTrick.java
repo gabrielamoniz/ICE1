82b30f2eeb4e71c0bcabb4d334281971d0dd5efe
gabrielamoniz <monizdea@sheridancollege.ca>
2023-02-01 08:56:21 -0500

    version 1 card details

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,15 +16,40 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
+            hand[i] = new Card();
+            hand[i].setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i].setValue(random.nextInt(13)+1);
+            hand[i].printInfo();
+            
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
+        System.out.println("Pick a card");
+        System.out.print("Suit:");
+        String pickedSuit = scanner.nextLine();
+        
+        System.out.print("Value:");
+        int pickedValue = scanner.nextInt();
+        
+        boolean found = false;
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].getSuit().equals(pickedSuit)&& hand[i].getValue()== 
+                    pickedValue){
+                found = true;
+                hand[i].printInfo();
+                break;
+            }
+        }
+            if(!found){
+                System.out.println("Card not found in the hand");
+            }
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
