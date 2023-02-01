f75ffc65828f92841f2557d092c5cfb3ed4b9f35
gabrielamoniz <monizdea@sheridancollege.ca>
2023-02-01 10:04:57 -0500

    version 3 card details

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -74,19 +74,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Gabriela");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Find a good job after I graduate");
+        System.out.println();
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Travelling");
+        System.out.println("-- Staying with my kids");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println();
 
         System.out.println();
         
