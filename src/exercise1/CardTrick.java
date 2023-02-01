89989506c7f218f49a5adc451abbd1911e1833ac
gabrielamoniz <monizdea@sheridancollege.ca>
2023-02-01 10:06:52 -0500

    version 4 card details

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -68,6 +68,7 @@ public class CardTrick {
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * @author Gabriela Moniz Feb 1, 2023
      */
     private static void printInfo() {
     
